2740ec5392d474dae7c0f26bfa637c7874dd671b
m stevens <bomaltennishal@outlook.com>
2025-12-15 17:04:15 +0100

    nieuw

---
+++ src/main/java/io/matthijs/raginaction/RaGinActionApplication.java
@@ -0,0 +1,13 @@
+package io.matthijs.raginaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaGinActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaGinActionApplication.class, args);
+	}
+
+}
